108fdb921195e3ce29d421cf5453d3b67aca8da5
porporboy <yoyozwassup@hotmail.com>
2017-04-15 04:53:24 -0700

    db changes

--- db.py
+++ db.py
@@ -1,34 +1,72 @@
 import pymysql.cursors
+import json
 
 
-# Connect to the database
-connection = pymysql.connect(host='ec2-34-209-104-246.us-west-2.compute.amazonaws.com',
-                             user='heyAI',
-                             password='heyAI',
-                             db='HeyAI',
+config = json.load(open('creds/db.json'))
+
+connection = pymysql.connect(host=config.get('host'),
+                             user=config.get('user'),
+                             password=config.get('pass'),
+                             port=config.get('port'),
+                             db=config.get('db'),
                              charset='utf8mb4',
                              cursorclass=pymysql.cursors.DictCursor)
 
-def fetch_one(sql, params):
+def read(sql, params):
+    """
+    DB read operations. fetchone() will be used if 'LIMIT 1' is present in the query. 
+    Otherwise, fetchall() will be used.
+    @param {str} sql: SQL querystring
+    @param {tuple} params: to be formatted into the SQL querystring
+    @return result: {list} and {dict} for fetchall() and fetchone() respectively.
+    """
     try:
-        # with connection.cursor() as cursor:
-        #     # Create a new record
-        #     cursor.execute(sql, ('webmaster@python.org', 'very-secret'))
-
-        # # connection is not autocommit by default. So you must commit to save
-        # # your changes.
-        # connection.commit()
-
         with connection.cursor() as cursor:
-            # Read a single record
             cursor.execute(sql, params)
-            result = cursor.fetchone()
+            if 'LIMIT 1' in sql.upper():
+                result = cursor.fetchone()
+            else:
+                result = cursor.fetchall()
+
             print('DB: executed {} ---- result: {}'.format(sql, result))
             return result
     finally:
         connection.close()
 
-def fetch_one(sql, params=None):
-    _fetch(sql, params)
 
-def fetch_all():
+def write(sql, params):
+    """
+    DB write operations.
+    @param {str} sql: SQL querystring
+    @param {tuple} params: to be formatted into the SQL querystring
+    @return result: {list} and {dict} for fetchall() and fetchone() respectively.
+    """
+    try:
+        with connection.cursor() as cursor:
+            # Create a new record
+            cursor.execute(sql, params)
+
+            print('DB: executed {}'.format(sql, result))
+
+        connection.commit()
+    finally:
+        connection.close()
+
+def insert_one(table_name, column_pairs):
+    """
+    Basic helper function to insert a new row into db.
+    @param {dict} column_pairs: a key-value pair representing the columns and their
+            corresponding values in a row.
+    """
+    sql = (
+        """
+        INSERT INTO %(table_name)s (%(columns)s) VALUES (%(values)s)
+        """
+        )
+
+    params = {
+        'table_name': '`{}`'.format(table_name),
+        'columns': '`{}`'.format('`,`'.join([str(key) for key in column_pairs.keys()])),
+        'values': ','.join([str(val) for val in column_pairs.values()]),
+    }
+    write(sql, params)
