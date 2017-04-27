7f844016f2015e1c38713cdf352c64865ccc4ec1
porporboy <yoyozwassup@hotmail.com>
2017-04-26 23:50:54 -0700

    Wed Apr 26 23:50:54 PDT 2017

--- start_server.sh
+++ start_server.sh
@@ -1,7 +1,2 @@
-if [ "$1" == "_run" ]; then
-	. virtualenv/bin/activate
-	python server.py
-else
-	nohup ./start_server.sh _run >/dev/null 2>log/main-process.log &
-	echo "python daemon started"
-fi
+. virtualenv/bin/activate
+python server.py
